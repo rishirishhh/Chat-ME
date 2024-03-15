5476016dbb11be0ff9c64da6ed97bab0ba215b80
rishi-19-04 <rishikavya19@gmail.com>
2024-03-15 19:30:39 +0530

    added the user service layer

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -9,6 +9,22 @@ type User struct {
 	Password string `json:"password" db:"email"`
 }
 
+type CreateUserReq struct {
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"email"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+}
+
 type Repository interface {
 	CreateUser(ctx context.Context, user *User) (*User, error)
+}
+
+type Service interface {
+	CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error)
 }
